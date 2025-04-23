842093ba6b3161593220c9b1a3c600b13f374e92
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 13:03:08 +0300

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   require 'jwt'
   include JsonWebToken
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request!
+
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+
+    decoded_token = decode_jwt_token(token)
+
+    if decoded_token && decoded_token['user_id']
+      @current_user = User.find_by(id: decoded_token['user_id'])
+    end
+
+    unless @current_user
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
