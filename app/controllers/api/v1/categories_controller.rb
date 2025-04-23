eebb7a5b044e972de4acf2ac301f92d52efcf5b4
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 13:16:20 +0300

    modify categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,51 @@
-class Api::V1::CategoriesController < ApplicationController
+# app/controllers/api/v1/categories_controller.rb
+module Api
+  module V1
+    class CategoriesController < ApplicationController
+      before_action :authenticate_request!
+      before_action :set_category, only: %i[show update destroy]
+
+      def index
+        @categories = Category.all
+        render json: @categories, status: :ok
+      end
+
+      def show
+        render json: @category, status: :ok
+      end
+
+      def create
+        @category = Category.new(category_params)
+
+        if @category.save
+          render json: @category, status: :created, location: category_url(@category)
+        else
+          render json: @category.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @category.update(category_params)
+          render json: @category, status: :ok
+        else
+          render json: @category.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @category.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_category
+        @category = Category.find!(params[:id])
+      end
+
+      def category_params
+        params.require(:category).permit(:name, :description, :color)
+      end
+    end
+  end
 end
