a89e481416c4c0def1999ecfb2f59eae8bb9acd6
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 12:15:42 +0300

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :first_name, :last_name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-
   validates :email, presence: true,
                     uniqueness: true,
                     format: {
