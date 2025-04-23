2795b06663cfdeb8132a1b09331518180db9457d
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 11:37:22 +0300

    validate user model attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: {
+                      with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/,
+                      message: 'must be a valid email address'
+                    }
+  validates :password, :password_confirmation, presence: true
 end
