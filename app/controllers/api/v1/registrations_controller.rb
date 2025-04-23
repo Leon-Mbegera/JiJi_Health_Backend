b24da555b309a6f46c60362824f3d722e4b94b82
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 12:02:23 +0300

    update user registration params

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -19,7 +19,7 @@ module Api
       private
 
       def registration_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
     end
   end
