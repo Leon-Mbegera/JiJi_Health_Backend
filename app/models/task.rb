6c694b85507db5fea25c8b7556d4079d37587f11
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 13:54:32 +0300

    update tasks controller and model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  enum priority: { low: 0, medium: 1, high: 2 }
-  enum status: { pending: 0, in_progress: 1, completed: 2 }
+enum :priority, { low: 0, medium: 1, high: 2 }, default: :low
+enum :status, { pending: 0, in_progress: 1, completed: 2 }, default: :pending
 
   validates :title, presence: true
   validates :due_date, presence: true
