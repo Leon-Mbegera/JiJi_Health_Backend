da8afb6750161a2f3f6040342534c52cd7f66c04
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-24 17:42:22 +0300

    validate task description

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,7 @@ enum :priority, { low: 0, medium: 1, high: 2 }, default: :low
 enum :status, { pending: 0, in_progress: 1, completed: 2 }, default: :pending
 
   validates :title, presence: true
+  validates :description, presence: true
   validates :due_date, presence: true
   validates :priority, presence: true
   validates :status, presence: true
